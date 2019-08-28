074df51fdcf6b81dd3c26eb9b3b0b294d430c4c9
Suchith J N <suchithjn22@gmail.com>
2019-08-28 20:02:05 +0530

    Add urlencoded body parser

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var PORT = process.env.PORT || 6666;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', function (req, res, next) {
   console.log(`${req.method} ${req.url} - ${req.ip}`);
   next();
